188d39e1c65e486e985f2e29a6f18cee57596087
federico berra <fede.berra@hotmail.it>
2020-01-19 23:52:46 +0100

    Fixed random problem in LGB split

--- src/algorithms/LGBsplit.h
+++ src/algorithms/LGBsplit.h
@@ -9,7 +9,7 @@ template <typename T>
 class LGBsplit : public LGB<T> {
  public:
   double perturbation;
-  LGBsplit() : LGB<T>{}, perturbation{0.01} {};
+  LGBsplit() : LGB<T>{}, perturbation{0.001} {};
   int run() {
     size_t N            = this->signal.size();
     size_t M            = this->nDimension * this->rate;
@@ -26,25 +26,27 @@ class LGBsplit : public LGB<T> {
 
     // init first centroid
     centroids.push_back(this->signal[(size_t)rand() % N]);
-    for (size_t i = 0; i < M; i++) {
+    for (size_t i = 0; i < M + 1; i++) {
       // LGB
       status = LGB<T>::run(centroids);
       // zero cluster check
       if (status <= 0)
         return status;
       // split
-      centroids = {};
-      for (Point<T> v : this->codebook) {
-        // random vector
-        std::for_each(temp.begin(), temp.end(), [per](T& element) {
-          element = rand() * per;
-        });
-        for (size_t j = 0; j < v.size(); j++) {
-          vp[j] = v[j] + temp[j];
-          vm[j] = v[j] - temp[j];
+      if (i != M) {
+        centroids = {};
+        for (Point<T> v : this->codebook) {
+          // random vector
+          std::for_each(temp.begin(), temp.end(), [per](T& element) {
+            element = ((double)rand() / (RAND_MAX)) * per;
+          });
+          for (size_t j = 0; j < v.size(); j++) {
+            vp[j] = v[j] + temp[j];
+            vm[j] = v[j] - temp[j];
+          }
+          centroids.push_back(vp);
+          centroids.push_back(vm);
         }
-        centroids.push_back(vp);
-        centroids.push_back(vm);
       }
     }
     return 1;
